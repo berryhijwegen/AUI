cd89032c77cb8e86d10ceac3dabb37663ca826d7
Berry Hijwegen <berry.hijwegen@student.hu.nl>
2019-02-19 10:51:25 +0100

    Finished JS

--- js/scripts.js
+++ js/scripts.js
@@ -8,30 +8,21 @@ $("#expectedP").submit(function(e) {
         if(tds[i].value != ""){
             totalECTS += parseInt(tds[i].value);
         }
-    }
+    };
 
     var today = new Date();
     var dd = today.getDate();
     var mm = today.getMonth() + 1;
     var yyyy = today.getFullYear();
     
-    if (dd < 10) {
-      dd = '0' + dd;
-    }
-    
-    if (mm < 10) {
-      mm = '0' + mm;
-    }
-
     rawStartDate = tds[2].value.split("-");
     startDate = [rawStartDate[2], rawStartDate[1], rawStartDate[0]];
     currentDate = [dd,mm,yyyy];
 
     var months = currentDate[1] - startDate[1]
     + (12 * (currentDate[2] - startDate[2]));
-    monthsLeft = (60 - totalECTS) / months;
+    ECTSPerMonth = totalECTS / months
+    monthsLeft = (60 - totalECTS) / ECTSPerMonth;
 
-    alert("Je hebt tot nu toe " + totalECTS + " ECTS behaald.\nJe Huidige studietempo is:\n" + totalECTS / months + " ECTS / mnd.\nOp basis van dit studietempo heb je " + monthsLeft + " mnd nodig\nvoor je P.");
+    alert("Je hebt tot nu toe " + totalECTS + " ECTS behaald.\nJe huidige studietempo is:\n" + ECTSPerMonth + " ECTS / mnd.\nOp basis van dit studietempo heb je " + monthsLeft + " mnd nodig\nvoor je P.");
 });
-
-
